2ab070cdc6d164a997b349a614972970f8876fc1
ishwar chaiser <ishwarchaiser@gmail.com>
2023-08-26 17:59:16 +0530

    project setup

---
+++ crow-service/src/test/java/com/icr7/crow/crowservice/CrowServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icr7.crow.crowservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
